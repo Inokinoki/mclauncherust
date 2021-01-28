41e814319ea4de8c6c9583fd3943ccf08c9046ed
Inoki <veyx.shaw@gmail.com>
2021-01-28 22:59:37 +0100

    Add a stateful list

--- src/tuiapp/app.rs
+++ src/tuiapp/app.rs
@@ -8,7 +8,7 @@ use tui::{
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
     text::Span,
-    widgets::{Block, BorderType, Borders},
+    widgets::{Block, BorderType, Borders, ListState},
     Terminal,
 };
 use std::{
@@ -127,5 +127,59 @@ impl TUIApp {
                 break;
             }
         }
+    }
+}
+
+
+pub struct StatefulList<T> {
+    pub state: ListState,
+    pub items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    pub fn new() -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items: Vec::new(),
+        }
+    }
+
+    pub fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn unselect(&mut self) {
+        self.state.select(None);
     }
 }
--- src/tuiapp/ui.rs
+++ src/tuiapp/ui.rs
@@ -1,4 +1,5 @@
 use crate::tuiapp::app::TUIAppState;
+use crate::tuiapp::app::StatefulList;
 
 use tui::{
     backend::Backend,
@@ -37,9 +38,44 @@ pub fn draw<B: Backend>(f: &mut Frame<B>, s: TUIAppState) {
         .title(Span::styled("Installed", Style::default().fg(Color::Green)));
     let all_version_block = Block::default().title("All")
         .borders(Borders::ALL);
+    // Iterate through all elements in the `items` app and append some debug text to it.
+    let mut statefulItems = StatefulList::with_items(vec![
+        "Item0",
+        "Item1",
+        "Item2",
+        "Item3",
+    ]);
+    let items: Vec<ListItem> = statefulItems
+        .items
+        .iter()
+        .map(|i| {
+            /*
+            for _ in 0..i.1 {
+                lines.push(Spans::from(Span::styled(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    Style::default().add_modifier(Modifier::ITALIC),
+                )));
+            }
+            */
+            ListItem::new(Spans::from(String::from(*i)))
+        })
+        .collect();
+
+    // Create a List from all list items and highlight the currently selected one
+    let items = List::new(items)
+        .block(all_version_block)
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol(">> ");
+
+    // We can now render the item list
+    f.render_stateful_widget(items, version_chunks[1], &mut statefulItems.state);
 
     f.render_widget(installed_version_block, version_chunks[0]);
-    f.render_widget(all_version_block, version_chunks[1]);
+    // f.render_widget(all_version_block, version_chunks[1]);
 
     /* Status chunks */
     let status_chunks = Layout::default()
